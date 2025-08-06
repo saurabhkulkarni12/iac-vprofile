29122104954fbfb4f23e80bb378b5b258eb31a20
saurabhkulkarni12 <skulkarni120191@gmail.com>
2025-08-06 14:25:05 +0530

    Bucket name and cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,12 +27,12 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vprofileactions3439"
+    bucket = "prodcrudappeks3439"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.12.2"
 }
 ##
 ##
